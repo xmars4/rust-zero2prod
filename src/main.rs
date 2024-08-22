67753855d0e51c28b158d2e35345fbf831cfb099
xmars4 <xtruongptit@gmail.com>
2024-08-22 14:33:48 +0700

    dynatic allocate port for test

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use zero2prod::run;
 use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("localhost:8000").expect("Can't bind port 8000");
+    let listener = TcpListener::bind("localhost:0").expect("Can't bind port 8000");
+    println!("Listening on port {}", listener.local_addr().unwrap().port());
     run(listener)?.await
 }
