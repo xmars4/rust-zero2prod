2fbac24af93bcd102b53521aebe4bcab9866d759
xmars4 <xtruongptit@gmail.com>
2024-08-22 14:33:14 +0700

    dynatic allocate port for test

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use actix_web::{web, App,  HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check( ) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server =HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use actix_web::{web, App,  HttpServer, Responder, HttpResponse};
-
-async fn health_check( ) -> impl Responder {
-    HttpResponse::Ok()
-}
-
+use zero2prod::run;
+use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("localhost:8000").expect("Can't bind port 8000");
+    run(listener)?.await
 }
