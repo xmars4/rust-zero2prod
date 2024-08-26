1c0012e6e654fbd2ec699793e755d6587c6be21c
xmars4 <xtruongptit@gmail.com>
2024-08-26 17:22:47 +0700

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
 use actix_web::{web, App,  HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use serde;
+
+
+#[derive(serde::Deserialize)]
+struct FormData{
+    email: String,
+    name: String
+}
 
 async fn health_check( ) -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server =HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
